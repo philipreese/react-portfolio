e0e12ebaa64bed72497ab6de6775dd738a33f2bb
Philip Reese <philipreese@mail.com>
2023-11-06 14:30:47 -0500

    Add Footer

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # react-portfolio
+
+To enable tailwind to build css for you:
+
+> npx tailwindcss -i ./src/index.css -o ./public/output.css --watch
+
+To run:
+
+> npm start
+
+To deploy:
+Create a Vercel account
--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MySkills from "./scenes/MySkills";
 import Projects from "./scenes/Projects";
 import Testimonials from "./scenes/Testimonials";
 import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
 import { useEffect, useState } from "react";
 import LineGradient from "./components/LineGradient";
 import useMediaQuery from "./hooks/useMediaQuery";
@@ -55,6 +56,7 @@ function App() {
       <div className="w-5/6 mx-auto md:h-full">
         <Contact />
       </div>
+      <Footer />
     </div>
   );
 }
